745fbdc55f140eb7484bffc32a4e63d081a3960f
jcanett1 <jcanett@pxg.com>
2025-06-12 11:57:06 -0700

    Create supabaseClient.js

---
+++ jss/supabaseClient.js
@@ -0,0 +1,8 @@
+const { createClient } = supabase;
+
+const supabaseUrl = "https://bwkvfwrrlizhqdpaxfmb.supabase.co"; 
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3a3Zmd3JybGl6aHFkcGF4Zm1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3NTIyODMsImV4cCI6MjA2NTMyODI4M30.6ryUGUVRcDtASw0s1RTnKwSA4ezn_I_oxHeuSWGmwFU";
+
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
+
+export default supabaseClient;
