6ee0f4ef3bb1e59f71239ebfb0ad41f664adfa89
jcanett1 <jcanett@pxg.com>
2025-06-13 09:47:35 -0700

    Update app.js

--- jss/app.js
+++ jss/app.js
@@ -1,11 +1,20 @@
 // js/app.js
 
-import * as UIController from './uiController.js';
 
 document.addEventListener("DOMContentLoaded", async () => {
   try {
-    console.log("Inicializando sistema...");
-    await UIController.init(); // Carga la interfaz desde Supabase
+    console.log("Ejecutando DOMContentLoaded...");
+
+    if (typeof UIController !== 'undefined') {
+      if (typeof UIController.init === 'function') {
+        await UIController.init(); // Inicializar la interfaz
+      } else {
+        throw new Error("UIController.init no es una función");
+      }
+    } else {
+      throw new Error("UIController no está definido");
+    }
+
   } catch (e) {
     console.error("Error durante la inicialización:", e.message);
   }
