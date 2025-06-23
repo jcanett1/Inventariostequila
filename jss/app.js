aa42285802cc954acf61331d7d00b1b44db41ebb
jcanett1 <jcanett@pxg.com>
2025-06-23 13:53:14 -0700

    Update app.js

--- jss/app.js
+++ jss/app.js
@@ -1,6 +1,6 @@
 import UIController from './uiController.js';
 import ChartController from './chartController.js';
-import { supabase } from './supabaseClient.js'; // Importa el cliente Supabase
+import supabase from './supabaseClient.js'; // Importa el cliente Supabase
 
 // Función para cargar datos de categorías desde Supabase
 async function fetchCategoriesData() {
