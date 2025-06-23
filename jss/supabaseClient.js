08e9975260e9f77db6bfa5f7ecb6ff07a4ca1f71
jcanett1 <jcanett@pxg.com>
2025-06-23 15:29:54 -0700

    Update supabaseClient.js

--- jss/supabaseClient.js
+++ jss/supabaseClient.js
@@ -1,14 +1,9 @@
-// supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
+// Usa esta importación en lugar de '@supabase/supabase-js'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 const supabaseUrl = 'https://bwkvfwrrlizhqdpaxfmb.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3a3Zmd3JybGl6aHFkcGF4Zm1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3NTIyODMsImV4cCI6MjA2NTMyODI4M30.6ryUGUVRcDtASw0s1RTnKwSA4ezn_I_oxHeuSWGmwFU'
 
-// Crear y exportar el cliente Supabase
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-// Exportación nombrada (recomendada)
 export { supabase }
-
-// O exportación por defecto (alternativa)
-// export default supabase
