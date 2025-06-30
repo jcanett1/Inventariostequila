f47f5a023c2fe45bf2f55d7e90f058dde6cf92ca
jcanett1 <jcanett@pxg.com>
2025-06-30 15:46:43 -0700

    Update supabaseClient.js

--- jss/supabaseClient.js
+++ jss/supabaseClient.js
@@ -3,6 +3,8 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const supabaseUrl = 'https://bwkvfwrrlizhqdpaxfmb.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3a3Zmd3JybGl6aHFkcGF4Zm1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3NTIyODMsImV4cCI6MjA2NTMyODI4M30.6ryUGUVRcDtASw0s1RTnKwSA4ezn_I_oxHeuSWGmwFU'
 
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: false // Recomendado para evitar problemas de caché
+  }
+})
